deb248c9fdc8000c259018240303c9bd9c53a281
marco <mj.jonker@live.nl>
2021-09-27 21:07:31 +0300

    remove strict

--- elevator.js
+++ elevator.js
@@ -2,9 +2,6 @@
  * Elevator
  * Data elevator
 **/
-
-'use strict'
-
 var ElevatorBase = require('data-elevator/lib/elevator-engine/elevator-base');
 var ConsoleLogger = require('data-elevator/lib/logger/console-logger');
 var Sqlite3LevelController = require('./lib/level-controllers/sqlite3-level-controller.js');
